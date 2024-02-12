36f17c402fca9f95ce15be6a716f001b895cf869
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2024-02-12 12:01:51 +0300

    "main"

--- models/hospital-management/doctor.models.js
+++ models/hospital-management/doctor.models.js
@@ -1,4 +1,46 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, {timestamps: true})
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    diagonsedWith :{
+        type: String,
+        required: true
+    },
+
+    address :{
+        type: String,
+        required: true
+    },
+
+
+    age :{
+        type: String,
+        required: true
+    },
+
+
+    bloodGroup :{
+        type: String,
+        required: true
+    },
+
+    gender :{
+
+    address :{
+        type: String,
+        enum : ["M",'F'],
+        required: true
+    },
+
+
+    admittedIn :{
+        type: mongoose.Schema.types.ObjectId,
+        ref: "Hospital",
+    },
+    }
+}, {timestamps: true})
 export const Doctor = mongoose.model("Doctor", doctorSchema)
